441533480be90bb72014cb41a0f985c5c2be2529
SummayaSiddiqui <summaya.siddiqui@keyin.com>
2024-11-27 16:50:57 -0330

    Added a function createTable() in db.js

--- db.js
+++ db.js
@@ -8,4 +8,22 @@ const pool = new Pool({
   port: 5432, // Default PostgreSQL port
 });
 
+async function createTable() {
+  const client = await pool.connect();
+  try {
+    await client.query(`
+        CREATE TABLE IF NOT EXISTS tasks (
+            task_id SERIAL PRIMARY KEY,
+            description TEXT NOT NULL,
+            status TEXT NOT NULL
+        );
+        `);
+    console.log("Table created successfully.");
+  } catch (err) {
+    console.error("Error creating table:", err);
+  } finally {
+    client.release();
+  }
+}
+
 module.exports = pool;
